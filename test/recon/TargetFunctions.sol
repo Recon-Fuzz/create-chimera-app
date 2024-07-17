84baa1624a05c64376d41fe8fe12f945d2406f50
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-07-17 10:40:28 -0300

    chore: add halmos stateful invariant test

--- test/recon/TargetFunctions.sol
+++ test/recon/TargetFunctions.sol
@@ -2,21 +2,27 @@
 pragma solidity ^0.8.0;
 
 import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
+import {vm} from "@chimera/Hevm.sol";
+import {SymTest} from "halmos-cheatcodes/SymTest.sol";
+
 import {BeforeAfter} from "./BeforeAfter.sol";
 import {Properties} from "./Properties.sol";
-import {vm} from "@chimera/Hevm.sol";
 
 abstract contract TargetFunctions is
     BaseTargetFunctions,
     Properties,
-    BeforeAfter
+    BeforeAfter,
+    SymTest
 {
+    /**
+        Echidna/Medusa Target Functions 
+    */
     function counter_increment() public {
         counter.increment();
     }
 
+    ///@notice example assertion test replicating testFuzz_SetNumber
     function counter_setNumber1(uint256 newNumber) public {
-        // example assertion test replicating testFuzz_SetNumber
         try counter.setNumber(newNumber) {
             if (newNumber != 0) {
                 eq(counter.number(), newNumber, "number != newNumber");
@@ -26,8 +32,8 @@ abstract contract TargetFunctions is
         }
     }
 
+    ///@notice same example assertion test as counter_setNumber1 using ghost variables
     function counter_setNumber2(uint256 newNumber) public {
-        // same example assertion test as counter_setNumber1 using ghost variables
         __before();
 
         counter.setNumber(newNumber);
@@ -39,8 +45,54 @@ abstract contract TargetFunctions is
         }
     }
 
-    /// @notice halmos symbolic execution test
+    /**
+     Halmos Symobolic Execution Tests
+    */
+    ///@notice checks an individual target function
+    // TODO: change this to call contract directly and add an assertion
     function check_Increment(uint256 newNumber) public {
         counter_setNumber1(newNumber);
+    }
+
+    ///@notice stateful symbolic execution test
+    function check_counter_symbolic(
+        bytes4[] memory selectors,
+        uint256 newValue
+    ) public {
+        for (uint256 i = 0; i < selectors.length; ++i) {
+            assumeValidSelector(selectors[i]);
+            assumeSuccessfulCall(
+                address(counter),
+                calldataFor(selectors[i], newValue)
+            );
+        }
+
+        t(counter.number() != 0, "number == 0");
+    }
+
+    ///@notice utility for returning the target functions selectors from the Counter contract
+    function assumeValidSelector(bytes4 selector) internal {
+        vm.assume(
+            selector == counter.setNumber.selector ||
+                selector == counter.increment.selector
+        );
+    }
+
+    ///@notice utility for making calls to the target contract
+    function assumeSuccessfulCall(address target, bytes memory data) internal {
+        (bool success, ) = target.call(data);
+        vm.assume(success);
+    }
+
+    ///@notice utility for getting calldata for a given function's arguments
+    function calldataFor(
+        bytes4 selector,
+        uint256 newValue
+    ) internal view returns (bytes memory) {
+        if (selector == counter.setNumber.selector) {
+            return abi.encodeWithSelector(selector, newValue);
+        } else if (selector == counter.increment.selector) {
+            return abi.encodeWithSelector(selector);
+        }
     }
 }

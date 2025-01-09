a21affb724a159e8f1eee149a41d4d709e30156a
gallo <alex@entreprenerd.xyz>
2025-01-09 17:59:00 +0100

    feat: MockERC20

---
+++ test/recon/mocks/MockERC20.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.25;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    uint8 private immutable _underlyingDecimals;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol) {
+        _underlyingDecimals = _decimals;
+    }
+
+    function mint(address to, uint256 value) public virtual {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) public virtual {
+        _burn(from, value);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _underlyingDecimals;
+    }
+}

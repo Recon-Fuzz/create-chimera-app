a0c517ae4206d8a3e84b85fe6dd3c1a587aee5f0
gallo <alex@entreprenerd.xyz>
2025-01-09 18:08:49 +0100

    feat: setup demo with an asset and actor

--- test/recon/Setup.sol
+++ test/recon/Setup.sol
@@ -1,13 +1,38 @@
 // SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.8.0;
 
+// Chimera deps
 import {BaseSetup} from "@chimera/BaseSetup.sol";
+import {vm} from "@chimera/Hevm.sol";
+
+// Managers
+import {ActorManager} from "./managers/ActorManager.sol";
+import {AssetManager} from "./managers/AssetManager.sol";
+
+// Your deps
 import "src/Counter.sol";
 
-abstract contract Setup is BaseSetup {
+abstract contract Setup is BaseSetup, ActorManager, AssetManager {
     Counter counter;
 
     function setup() internal virtual override {
+        // New Actor, beside address(this)
+        _addActor(address(0x411c3));
+        _newAsset(18); // New 18 decimals token
+
         counter = new Counter();
+
+        // TODO: Standardize Mint and allowances to all actors
+    }
+
+    // NOTE: LIMITATION You can use these modifier only for one call, so use them for BASIC TARGETS
+    modifier asAdmin {
+        vm.prank(address(this));
+        _;
+    }
+
+    modifier asActor {
+        vm.prank(_getActor());
+        _;
     }
 }

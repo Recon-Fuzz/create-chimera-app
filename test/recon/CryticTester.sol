9bbf135bc24b7d12fc5957ac758c1cb37a0c764b
gallo <alex@entreprenerd.xyz>
2025-01-28 11:31:58 +0100

    feat: 16 cores

--- test/recon/CryticTester.sol
+++ test/recon/CryticTester.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {TargetFunctions} from "./TargetFunctions.sol";
 import {CryticAsserts} from "@chimera/CryticAsserts.sol";
 
-// echidna . --contract CryticTester --config echidna.yaml
+// echidna . --contract CryticTester --config echidna.yaml --format text --workers 16 --test-limit 1000000
 // medusa fuzz
 contract CryticTester is TargetFunctions, CryticAsserts {
     constructor() payable {

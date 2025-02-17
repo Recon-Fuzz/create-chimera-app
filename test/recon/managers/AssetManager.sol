a192943c9a8bbbe1961ed8864460b7145c37f57f
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-02-17 15:42:00 -0300

    refactor: AssetManager for simplicity + add natspec

--- test/recon/managers/AssetManager.sol
+++ test/recon/managers/AssetManager.sol
@@ -7,12 +7,15 @@ import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet
 
 import {MockERC20} from "../mocks/MockERC20.sol";
 
+/// @dev Source of truth for the assets being used in the test
+/// @notice No assets should be used in the suite without being added here first
 abstract contract AssetManager {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    // The current target for this set of variables
+    /// @notice The current target for this set of variables
     address private __asset;
 
+    /// @notice The list of all assets being used
     EnumerableSet.AddressSet private _assets;
 
     // If the current target is address(0) then it has not been setup yet and should revert
@@ -22,7 +25,7 @@ abstract contract AssetManager {
     // Enable only added assets
     error NotAdded();
 
-    // Note: use this function _to get the current active asset
+    /// @notice Returns the current active asset
     function _getAsset() internal view returns (address) {
         if (__asset == address(0)) {
             revert NotSetup();
@@ -31,28 +34,23 @@ abstract contract AssetManager {
         return __asset;
     }
 
+    /// @notice Returns all assets being used
     function _getAssets() internal view returns (address[] memory) {
         return _assets.values();
     }
 
+    /// @notice Creates a new asset and adds it to the list of assets
+    /// @param decimals The number of decimals for the asset
+    /// @return The address of the new asset
     function _newAsset(uint8 decimals) internal returns (address) {
         address asset_ =  address(new MockERC20("Test Token", "TST", decimals)); // If names get confusing, concatenate the decimals to the name
         _addAsset(asset_);
-        _enableAsset(asset_);
+        __asset = asset_; // sets the asset as the current asset
         return asset_;
     }
 
-    function _enableAsset(address target) internal {
-        if (!_assets.contains(target)) {
-            revert NotAdded();
-        }
-        __asset = target;
-    }
-
-    function _disableAsset(address asset_) internal {
-        // Here there are actions that would be needed when removing a asset.
-    }
-
+    /// @notice Adds an asset to the list of assets
+    /// @param target The address of the asset to add
     function _addAsset(address target) internal {
         if (_assets.contains(target)) {
             revert Exists();
@@ -61,21 +59,29 @@ abstract contract AssetManager {
         _assets.add(target);
     }
 
+    /// @notice Removes an asset from the list of assets
+    /// @param target The address of the asset to remove
     function _removeAsset(address target) internal {
+        if (!_assets.contains(target)) {
+            revert NotAdded();
+        }
+
         _assets.remove(target);
     }
 
-    // Note: expose this function _in `TargetFunctions` for asset switching
+    /// @notice Switches the current asset based on the entropy
+    /// @param entropy The entropy to choose a random asset in the array for switching
     function _switchAsset(uint256 entropy) internal {
-        _disableAsset(__asset); // NOTE: May not be necessary
-
         address target = _assets.at(entropy % _assets.length());
-        _enableAsset(target);
+        __asset = target;
     }
 
-    /// === Approve and mint for all === ///
+    /// === Approve & Mint Asset === ///
 
-    // mint initial balance and approve allowances for the active asset
+    /// @notice Mint initial balance and approve allowances for the active asset
+    /// @param actorsArray The array of actors to mint the asset to
+    /// @param approvalArray The array of addresses to approve the asset to
+    /// @param amount The amount of the asset to mint
     function _finalizeAssetDeployment(address[] memory actorsArray, address[] memory approvalArray, uint256 amount) internal {
         _mintAssetToAllActors(actorsArray, amount);
         for(uint256 i; i < approvalArray.length; i++) {
@@ -83,7 +89,10 @@ abstract contract AssetManager {
         }
     }
 
-    function _mintAssetToAllActors(address[] memory actorsArray, uint256 amount) internal {
+    /// @notice Mint the asset to all actors
+    /// @param actorsArray The array of actors to mint the asset to
+    /// @param amount The amount of the asset to mint
+    function _mintAssetToAllActors(address[] memory actorsArray, uint256 amount) private {
         // mint all actors
         address asset = _getAsset();
         for (uint256 i; i < actorsArray.length; i++) {
@@ -92,7 +101,10 @@ abstract contract AssetManager {
         }
     }
 
-    function _approveAssetToAddressForAllActors(address[] memory actorsArray, address addressToApprove) internal {
+    /// @notice Approve the asset to all actors
+    /// @param actorsArray The array of actors to approve the asset from
+    /// @param addressToApprove The address to approve the asset to
+    function _approveAssetToAddressForAllActors(address[] memory actorsArray, address addressToApprove) private {
         // approve to all actors
         address asset = _getAsset();
         for (uint256 i; i < actorsArray.length; i++) {

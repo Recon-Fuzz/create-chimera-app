c8b36533c6e0e45000f08d50043e24e77a637db1
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-02-17 15:18:13 -0300

    chore: natspec for ActorManager

--- test/recon/managers/ActorManager.sol
+++ test/recon/managers/ActorManager.sol
@@ -5,11 +5,15 @@ import {BaseSetup} from "@chimera/BaseSetup.sol";
 import {vm} from "@chimera/Hevm.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
+/// @dev This is the source of truth for the actors being used in the test
+/// @notice No actors should be used in the suite without being added here first
 abstract contract ActorManager {
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    ///@notice The current actor being used
     address private _actor;
 
+    ///@notice The list of all actors being used
     EnumerableSet.AddressSet private _actors;
 
     // If the current target is address(0) then it has not been setup yet and should revert
@@ -21,22 +25,23 @@ abstract contract ActorManager {
     // Do not allow the default actor
     error DefaultActor();
 
+    /// @notice address(this) is the default actor
     constructor() {
-        // address(this) is the default actor
         _actors.add(address(this));
         _actor = address(this);
     }
 
-    // use this function to get the current active actor
+    /// @notice Returns the current active actor
     function _getActor() internal view returns (address) {
        return _actor;
     }
 
-    // Get regular users
+    /// @notice Returns all actors being used
     function _getActors() internal view returns (address[] memory) {
         return _actors.values();
     }
 
+    /// @notice Adds an actor to the list of actors
     function _addActor(address target) internal {
         if (_actors.contains(target)) {
             revert ActorExists();
@@ -49,6 +54,7 @@ abstract contract ActorManager {
         _actors.add(target);
     }
 
+    /// @notice Removes an actor from the list of actors
     function _removeActor(address target) internal {
         if (!_actors.contains(target)) {
             revert ActorNotAdded();
@@ -61,7 +67,9 @@ abstract contract ActorManager {
         _actors.remove(target);
     }
 
-    // Note: expose this function _in `TargetFunctions` for actor switching
+    /// @dev Expose this in the `TargetFunctions` contract to let the fuzzer switch actors
+    /// @notice Switches the current actor based on the entropy
+    /// @param entropy The entropy to choose a random actor in the array for switching
     function _switchActor(uint256 entropy) internal {
         address target = _actors.at(entropy % _actors.length());
         _actor = target;
